52acf75a50e7965ca75ff8d7f32523d21b86b1db
Hieu Pham <phamgiatrihieu@gmail.com>
2025-02-17 13:16:25 +0700

    chang API url

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { Cron, CronExpression } from '@nestjs/schedule';
+
+const url = 'https://portfolio-be-mongodb-9jn4.onrender.com';
 @Injectable()
 export class AppService {
-  private readonly apiUrl =
-    'https://portfolio-be-mongodb-hjo1.onrender.com/api/v1/portfolios/home/my-portfolio';
+  private readonly apiUrl = `${url}/api/v1/portfolios/home/my-portfolio`;
   getHello(): string {
     return 'Biết ngay mà thế nào củng vào để xem có Hello World không chứ gì ??';
   }
