3785c16da5fcd0843fe291034e13fe4e97158529
PhamGiaTriHieu <141018793+PhamGiaTriHieu@users.noreply.github.com>
2025-04-14 21:28:43 +0700

    [add] docker-compose file

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios';
-import { Cron, CronExpression } from '@nestjs/schedule';
+// import axios from 'axios';
+// import { Cron, CronExpression } from '@nestjs/schedule';
 
-const url = 'https://portfolio-be-mongodb-74c5.onrender.com';
+// const url = 'https://portfolio-be-mongodb-74c5.onrender.com';
 @Injectable()
 export class AppService {
-  private readonly apiUrl = `${url}/api/v1/portfolios/home/my-portfolio`;
+  // private readonly apiUrl = `${url}/api/v1/portfolios/home/my-portfolio`;
   getHello(): string {
     return 'Biết ngay mà thế nào củng vào để xem có Hello World không chứ gì ??';
   }
 
-  @Cron(CronExpression.EVERY_5_MINUTES)
-  autoCallPortfolio() {
-    this.callApi();
-  }
+  // @Cron(CronExpression.EVERY_5_MINUTES)
+  // autoCallPortfolio() {
+  //   this.callApi();
+  // }
 
-  async callApi() {
-    try {
-      const response = await axios.get(this.apiUrl);
-      console.log(response.data);
-      return response.data;
-    } catch (error) {
-      console.error('Error calling API:', error);
-    }
-  }
+  // async callApi() {
+  //   try {
+  //     const response = await axios.get(this.apiUrl);
+  //     console.log(response.data);
+  //     return response.data;
+  //   } catch (error) {
+  //     console.error('Error calling API:', error);
+  //   }
+  // }
 }
